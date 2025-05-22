3fcf28d83eaaefc3f953089e155b691adfec564a
MyataEtoki <baksyanya2006@gmail.com>
2025-05-23 01:10:02 +0300

    upd добила УД + АИС

--- 4_semester/УД/6-7 лр/second_query.sql
+++ 4_semester/УД/6-7 лр/second_query.sql
@@ -1,5 +1,13 @@
 USE home_library;
---  В.17. 2) Получить информацию о всех книгах, содержащих детективы.
-SELECT * FROM books
+--  В.17. 2) Получить информацию о всех книгах, содержащих детективы. (+автор)
+SELECT 
+	books.book_id, 
+    books.book_name, 
+	a.first_name, 
+    a.last_name, 
+    book_genre.genre_name 
+FROM books
 JOIN book_genre ON books.book_genre_id = book_genre.genre_id
+JOIN authors_records ar ON books.book_id = ar.book_id
+JOIN authors a ON ar.author_id = a.author_id
 WHERE book_genre.genre_name = 'Детектив';
--- 4_semester/УД/6-7 лр/third_query.sql
+++ 4_semester/УД/6-7 лр/third_query.sql
@@ -1,6 +1,18 @@
 USE home_library;
 --  В.17. 3) Получить список книг, взятых вами в долг.
-SELECT * FROM debts d
+SELECT 
+	b.book_id, 
+	b.book_name, 
+    d.date_start, 
+    d.date_end, 
+	a.first_name,
+    a.medium_name,
+    a.last_name
+FROM debts d
 JOIN debts_records dr ON d.debt_id = dr.debt_id
 JOIN books b ON dr.book_id = b.book_id
+
+JOIN authors_records ar ON b.book_id = ar.book_id
+JOIN authors a ON ar.author_id = a.author_id
+
 WHERE d.friend_id = 11;
