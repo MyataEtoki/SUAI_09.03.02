28a84e7573838eef7ff97dda23aed4c39d318108
MyataEtoki <baksyanya2006@gmail.com>
2025-06-27 11:37:38 +0300

    ааа

--- 4_semester/ТП/ТП-лр4-5/actlly_5_lr.py
+++ 4_semester/ТП/ТП-лр4-5/actlly_5_lr.py
@@ -5,11 +5,13 @@ import heapq
 
 
 def load_metro_map(json_file):
+    """Парсер json схемы метро"""
     with open(json_file, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def metro_map_to_dict(metro_data):
+    """Преобразуем текстовою схему в удобный словарь"""
     metro_dict = {}
     for line in metro_data['lines']:
         line_name = line['name']
@@ -20,14 +22,18 @@ def metro_map_to_dict(metro_data):
 
 
 def travel_time_dict(metro_data):
+    """Для расчёта времени в пути"""
     return metro_data.get("travel_time", {})
 
 
 def find_shortest_path(metro_data, start_station, end_station, transfer_time=5, default_travel_time=3):
+
     travel_times = travel_time_dict(metro_data)
     station_lines = {}
     line_stations = {}
 
+    #TODO: надо объединить с metro_map_to_dict()
+    """считываем инфу о станциях"""
     for line in metro_data['lines']:
         line_name = line['name']
         stations = [station if isinstance(station, str) else station.get(
@@ -39,6 +45,7 @@ def find_shortest_path(metro_data, start_station, end_station, transfer_time=5, 
     heap = []
     visited = {}
 
+    # забиваем очередь кортежами из линий, где есть стартовая станция
     for line in station_lines.get(start_station, []):
         heapq.heappush(heap, (0, 0, start_station,
                        line, [(start_station, line)]))
@@ -48,15 +55,16 @@ def find_shortest_path(metro_data, start_station, end_station, transfer_time=5, 
     best_transfers = None
 
     while heap:
+        # берём путь с мин временем
         cost, transfers, station, line, path = heapq.heappop(heap)
         key = (station, line)
-        if key in visited and visited[key] <= cost:
+        if key in visited and visited[key] <= cost: #скипаем путь, если быстрее уже было
             continue
         visited[key] = cost
 
         if station == end_station:
             transfer_count = sum(1 for i in range(
-                1, len(path)) if path[i][1] != path[i-1][1])
+                1, len(path)) if path[i][1] != path[i-1][1]) #считаем пересадки ипо изменению линии в пути
             if cost < best_cost:
                 best_cost = cost
                 best_path = path
@@ -85,7 +93,8 @@ def find_shortest_path(metro_data, start_station, end_station, transfer_time=5, 
     return station_path, best_transfers, best_cost
 
 
-def user_shortest_route(metro_data):
+def user_data(metro_data):
+    """Работа с выводом инфы в консоль из файла метро, ввод из консоли точек маршрута пользователем"""
     stations = set()
     for line in metro_data['lines']:
         for station in line['stations']:
@@ -107,11 +116,6 @@ def user_shortest_route(metro_data):
         print(f"Всего ехать: {total_time} минуток")
 
 
-def main():
-
-    metro_data = load_metro_map('metro.json')
-    user_shortest_route(metro_data)
-
-
 if __name__ == "__main__":
-    main()
+    metro_data = load_metro_map('metro.json')
+    user_data(metro_data)
